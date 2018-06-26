4ce9ca23b1332ae86276c2c4c3bfbfbec206d931
Yash <code.yash@gmail.com>
2018-06-26 20:03:27 +0000

    packages

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
+#Before sudo
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+
 #sudo su
-pacman -S opera firefox geany git redshift 
+pacman -S opera firefox geany git redshift rxvt-unicode rofi
+
+# New fav font Shape: Menlo
+pacman -S ttf-iosevka-ss04
 
 #will continue to update it
+
+#Heavy ones
+pacman -S intellij-idea-community-edition pycharm-community-edition
+
+yay -S sublime-text-dev
+
+#Setup node
